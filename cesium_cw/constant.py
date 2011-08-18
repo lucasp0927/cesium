418ea458521b2b9bb34d0adbaa86fc4ff80017e2
Lucas <lucasp0927@gmail.com>
2011-08-18 15:26:01 +0800

    fix typo and add constant to cw

---
+++ cesium_cw/constant.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2
+""" Collection of physical constants
+
+Main source: http://physics.nist.gov/cuu/Constants/index.html
+"""
+
+def AMU():
+    """ Atomic mass unit, kg """
+    return 1.660538782e-27
+
+def AVOGADRO():
+    """ Avogadro constant, mol^-1 """
+    return 6.02214179e23
+
+def BOLTZMANN():
+    """ Boltzmann constant, J K^-1 """
+    return 1.3806504e-23
+
+def ELECTRONMASS():
+    """ Electron mass, kg """
+    return 9.10938215e-31
+
+def ELEMENTARYCHARGE():
+    """ Elementary charge, C """
+    return 1.602176487e-19
+
+def PLANCK():
+    """ Planck constant, J s """
+    return 6.62606896e-34
+
+def HBAR():
+    """ Planck constant divided by 2 pi, J s """
+    return 1.054571628e-34
+
+def RYDBERG():
+    """ Ridberg constant, m^-1 """
+    return 10973731.568527
+
+def SPEEDOFLIGHT():
+    """ Speed of light in vacuum, m s^-1 """
+    return 299792458
+
