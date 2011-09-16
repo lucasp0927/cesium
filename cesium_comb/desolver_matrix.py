0b45ae96c2933c41fec1f9d88f6c1c5ec4f28a1b
Lucas <lucasp0927@gmail.com>
2011-09-16 15:06:11 +0800

    10x faster

--- cesium_comb/desolver_matrix.py
+++ cesium_comb/desolver_matrix.py
@@ -28,67 +28,41 @@ class DESolver_matrix(object):
 	    
     def solve(self, period,N):
 	resultr = np.identity(N,dtype = np.float64)
-        resultr_gpu = gpuarray.to_gpu(resultr)
-	resulti = np.zeros([N,N],dtype = np.float64)
-        resulti_gpu = gpuarray.to_gpu(resulti)
-	resultr_tmp = np.identity(N,dtype = np.float64)
-        resultr_tmp_gpu = gpuarray.to_gpu(resultr_tmp)
-	resulti_tmp = np.identity(N,dtype = np.float64)
-        resulti_tmp_gpu = gpuarray.to_gpu(resulti_tmp)
-	I = np.identity(N,dtype = np.float64)
-        I_gpu = gpuarray.to_gpu(I)
+	resulti = np.zeros([N,N],dtype = np.float64)	
+	I = np.identity(N,dtype = np.float64)	
 	# E_arr = np.array(self.E_arr)
 	# matrix_electric = self.matrix_electric
 	# matrix_static = self.matrix_static
-	Her = np.array(self.matrix_electric.real)
-        Her_gpu = gpuarray.to_gpu(Her)
-	Hei = np.array(self.matrix_electric.imag)
-        Hei_gpu = gpuarray.to_gpu(Hei)
-	Hsr = np.array(self.matrix_static.real)
-        Hsr_gpu = gpuarray.to_gpu(Hsr)
-	Hsi = np.array(self.matrix_static.imag)
-        Hsi_gpu = gpuarray.to_gpu(Hsi)
+	Her = self.matrix_electric.real
+	Hei = self.matrix_electric.imag
+	Hsr = self.matrix_static.real
+	Hsi = self.matrix_static.imag
 	#python version
 	k2r = np.zeros([self.N,self.N],dtype = np.float64)
-        k2r_gpu = gpuarray.to_gpu(k2r)
 	k2i = np.zeros([self.N,self.N],dtype = np.float64)
-        k2i_gpu = gpuarray.to_gpu(k2i)
 	k3r = np.zeros([self.N,self.N],dtype = np.float64)
-        k3r_gpu = gpuarray.to_gpu(k3r)
 	k3i = np.zeros([self.N,self.N],dtype = np.float64)
-        k3i_gpu = gpuarray.to_gpu(k3i)
 	k4r = np.zeros([self.N,self.N],dtype = np.float64)
-        k4r_gpu = gpuarray.to_gpu(k4r)
-	k4i = np.zeros([self.N,self.N],dtype = np.float64)
-        k4i_gpu = gpuarray.to_gpu(k4i)
-	tmpr = np.zeros([self.N,self.N],dtype = np.float64)
-        tmpr_gpu = gpuarray.to_gpu(tmpr)
-	tmpi = np.zeros([self.N,self.N],dtype = np.float64)
-        tmpi_gpu = gpuarray.to_gpu(tmpi)
-        
+	k4i = np.zeros([self.N,self.N],dtype = np.float64)			
         for i in xrange(0,self.fine_step,2):
 	    print i
 	    t = time.time()
-	    k1r_gpu = (Her_gpu*self.E_arr[period][i] + Hsr_gpu)*self.dt
-	    k1i_gpu = (Hei_gpu*self.E_arr[period][i] + Hsi_gpu)*self.dt
-	    tmpr_gpu = Her_gpu*self.E_arr[period][i+1]+Hsr_gpu
-	    tmpi_gpu = Hei_gpu*self.E_arr[period][i+1]+Hsi_gpu
-            self.gpu.matrix_mul(tmpr_gpu,tmpi_gpu,I_gpu+k1r_gpu*0.5,k1i_gpu*0.5,k2r_gpu,k2i_gpu)
-	    k2r_gpu = k2r_gpu*self.dt
-	    k2i_gpu = k2i_gpu*self.dt
-            self.gpu.matrix_mul(tmpr_gpu,tmpi_gpu,I_gpu+k2r_gpu*0.5,k2i_gpu*0.5,k3r_gpu,k3i_gpu)
-	    k3r_gpu = k3r_gpu * self.dt
-	    k3i_gpu = k3i_gpu * self.dt
-	    tmpr_gpu = Her_gpu*self.E_arr[period][i+2]+Hsr_gpu
-	    tmpi_gpu = Hei_gpu*self.E_arr[period][i+2]+Hsi_gpu            
-            self.gpu.matrix_mul(tmpr_gpu,tmpi_gpu,I_gpu+k3r_gpu,k3i_gpu,k4r_gpu,k4i_gpu)
-	    k4r_gpu = k4r_gpu * self.dt
-	    k4i_gpu = k4i_gpu * self.dt	    
-	    self.gpu.matrix_mul(I_gpu+1.0/6.0*(k1r_gpu+2.0*k2r_gpu+2.0*k3r_gpu+k4r_gpu),1.0/6.0*(k1i_gpu+2.0*k2i_gpu+2.0*k3i_gpu+k4i_gpu),resultr_gpu,resulti_gpu,resultr_tmp_gpu,resulti_tmp_gpu)#notice here!!
-            resultr_gpu = resultr_tmp_gpu
-            resulti_gpu = resulti_tmp_gpu            
+	    k1r = (Her*self.E_arr[period][i] + Hsr)*self.dt
+	    k1i = (Hei*self.E_arr[period][i] + Hsi)*self.dt
+	    tmpr = Her*self.E_arr[period][i+1]+Hsr
+	    tmpi = Hei*self.E_arr[period][i+1]+Hsi
+            self.gpu.matrix_mul(tmpr,tmpi,I+k1r*0.5,k1i*0.5,k2r,k2i)
+	    k2r *= self.dt
+	    k2i *= self.dt
+            self.gpu.matrix_mul(tmpr,tmpi,I+k2r*0.5,k2i*0.5,k3r,k3i)
+	    k3r *= self.dt
+	    k3i *= self.dt	    
+            self.gpu.matrix_mul((Her*self.E_arr[period][i+2]+Hsr),(Hei*self.E_arr[period][i+2]+Hsi),I+k3r,k3i,k4r,k4i)
+	    k4r *= self.dt
+	    k4i *= self.dt	    
+	    self.gpu.matrix_mul(I+1.0/6.0*(k1r+2.0*k2r+2.0*k3r+k4r),1.0/6.0*(k1i+2.0*k2i+2.0*k3i+k4i),resultr,resulti,resultr,resulti)
 	    print time.time() - t
-        return resultr_gpu.get() + resulti_gpu.get()*1j
+        return resultr + resulti*1j
 
 
         # for i in xrange(0,self.fine_step,2):
--- cesium_comb/gpu_matrix_test.py
+++ cesium_comb/gpu_matrix_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python2
-
 from gpu_matrix import GPU_Matrix
 import numpy as np
 import time 
@@ -11,8 +10,12 @@ c = np.random.randn(MATRIX_SIZE, MATRIX_SIZE).astype(np.float64)
 d = np.random.randn(MATRIX_SIZE, MATRIX_SIZE).astype(np.float64)
 A = a+b*1j
 B = c+d*1j
+
 t = time.time()
-C = gpu.matrix_mul(a,b,c,d)
-print time.time() - t
-
-
+f,g =  gpu.matrix_mul(a,b,c,d)
+result_g = f+g*1j
+print 'gpu time: ', time.time() - t
+t = time.time()
+result_c = np.dot(A,B)
+print 'cpu time: ',time.time() - t
+print np.linalg.norm(result_c- result_g)
